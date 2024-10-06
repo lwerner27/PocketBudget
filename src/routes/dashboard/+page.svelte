fba4703d1f0832387be10c520f1838282513f18b
Lucas Werner <lwerner27@gmail.com>
2024-10-06 16:15:36 +0000

    Stopped using toFixed() in calculation.

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	export let data;
 	const { budget } = data.user;
-	const alertColor = () => {
-		if (data.weeklyTotal >= (budget * 0.66).toFixed(2)) {
+	const alertColorCalculator = () => {
+		if (data.weeklyTotal >= budget * 0.66) {
+			console.log('Hello');
 			return 'alert-error';
-		} else if (data.weeklyTotal >= (budget * 0.33).toFixed(2)) {
+		} else if (data.weeklyTotal >= budget * 0.33) {
 			return 'alert-warning';
 		} else {
 			return 'alert-success';
@@ -14,7 +15,7 @@
 
 <div class="container mx-auto">
 	<div class="w-11/12 mx-auto mt-5">
-		<div role="alert" class={`alert ${alertColor()}`}>
+		<div role="alert" class={`alert ${alertColorCalculator()}`}>
 			<span>Currently Weekly Total: ${data.weeklyTotal}</span>
 		</div>
 	</div>
